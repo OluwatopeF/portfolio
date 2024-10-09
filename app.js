ae1d101091316f254d32ba7dbf2b69ad0a3187ed
OluwatopeF <topeumar@gmail.com>
2024-10-09 15:08:21 -0400

    update 10/9/2024

--- app.js
+++ app.js
@@ -6,6 +6,16 @@ const contactSection = document.querySelector("footer");
 // Hide project section initially
 projChange.style.maxHeight = "0";
 
+
+// Returns home(resets style changes)
+function returnHome(){
+    if(bodyChange.classList.contains('expanded') || projChange.style.maxHeight != 0 || contactSection.style.maxHeight != 0){
+        bodyChange.classList.remove('expanded');
+        projChange.style.maxHeight = "0"; 
+        contactSection.style.maxHeight = "0"; 
+    }
+}
+
 // Opens Project section (with animation)
 function openProjects() {
     if (bodyChange.classList.contains('expanded')) {
@@ -21,7 +31,14 @@ function openProjects() {
 
 // Opens Contact section (with animation)
 function openContact() {
-    bodyChange.classList.remove('expanded');
-    projChange.style.maxHeight = "0";
-    contactSection.style.maxHeight = "690px";
+    if(bodyChange.classList.contains('expanded')){
+        bodyChange.classList.remove('expanded');
+        projChange.style.maxHeight = "0";
+        contactSection.style.maxHeight = "690px";
+    } else{
+        bodyChange.classList.add('expanded');
+        contactSection.style.maxHeight = "687px"; 
+        projChange.style.maxHeight = "0"; 
+    }
 }
+    
