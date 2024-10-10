1219b2aede78a7a4a0869f60a0577fddc8102608
OluwatopeF <topeumar@gmail.com>
2024-10-10 19:07:52 -0400

    update 10/10/2024

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ const bodyChange = document.body;
 const projChange = document.querySelector(".project");
 const contactSection = document.querySelector("footer");
 
-// Hide project section initially
+// need id for inner div boxes (will set to none)
+const contactBox = document.getElementById("contactBox");
+
+// Hide div boxes initially
 projChange.style.maxHeight = "0";
+contactBox.style.display = "none";
 
 
 // Returns home(resets style changes)
@@ -19,12 +23,17 @@ function returnHome(){
 // Opens Project section (with animation)
 function openProjects() {
     if (bodyChange.classList.contains('expanded')) {
+        // for repetition
         bodyChange.classList.remove('expanded');
+        // to close
         projChange.style.maxHeight = "0"; 
-        contactSection.style.maxHeight = "0"; 
+        
     } else {
+        // for repetition
         bodyChange.classList.add('expanded');
-        projChange.style.maxHeight = "687px"; 
+        // REQUIRED FOR ANIMATION
+        projChange.style.maxHeight = "687px";
+        // to keep contacts closed
         contactSection.style.maxHeight = "0"; 
     }
 }
@@ -32,13 +41,22 @@ function openProjects() {
 // Opens Contact section (with animation)
 function openContact() {
     if(bodyChange.classList.contains('expanded')){
+        // to shrink
         bodyChange.classList.remove('expanded');
+        contactBox.style.maxHeight = "0";
+        // too sudden
+        // contactBox.style.display = "none";
         projChange.style.maxHeight = "0";
-        contactSection.style.maxHeight = "690px";
+        contactSection.style.maxHeight = "0";
     } else{
+        // to open
         bodyChange.classList.add('expanded');
+        // will change to match requireed height:
+        contactBox.style.maxHeight = "90px";
+        contactBox.style.display = "flex";
         contactSection.style.maxHeight = "687px"; 
         projChange.style.maxHeight = "0"; 
     }
 }
-    
+
+
