08567127bd46a59bb0a462eb5d0d2b6a70f0b3f3
OluwatopeF <topeumar@gmail.com>
2024-10-18 15:15:51 -0400

    update 10/18/2024

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// import titles from './desc.json';
+
 // Get body and project elements
 const bodyChange = document.body;
 const projChange = document.querySelector(".project");
@@ -68,3 +70,37 @@ function openLinkedin(){
     window.open('https://www.linkedin.com/in/oluwatope-folorunso-523222207/')
 }
 
+
+// using JSON for convenient input of project description in future
+
+
+
+
+fetch('./data.json')
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+        const innerProj = document.getElementById('innerProj');
+        innerProj.innerHTML = '';
+
+        // function to create a new div for each project in div
+        data.forEach(item => {
+            const projDiv = document.createElement('div');
+            projDiv.className = 'projectItems';
+
+            const itemlink = item.link;
+
+
+            // NOTE: {`} is not the same as {'}
+            projDiv.innerHTML = `
+                <img src = "${item.image}" alt="${item.title}" style = "width:100%; height:40%;">
+                <h1>${item.title}</h1>
+                <br>
+                <p>${item.description}</p>
+                <br>
+                <a href="${itemlink}" target="_blank">Open link</a>
+                `;
+
+            innerProj.appendChild(projDiv);
+        });
+    });
